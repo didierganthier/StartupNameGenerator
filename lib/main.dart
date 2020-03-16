295c51b5867093c799ce5938fbb5fc6b36f32706
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-03-15 22:13:47 -0400

    Favorite Icon Added

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class RandomWords extends StatefulWidget{
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair>_saved = Set<WordPair>();
+  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,11 +48,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair){
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
           pair.asPascalCase,
           style: _biggerFont
-      )
+      ),
+      trailing: Icon(
+        alreadySaved?Icons.favorite : Icons.favorite_border,
+        color: alreadySaved? Colors.red: null,
+      ),
+      onTap: (){
+        if(alreadySaved)
+          {
+            _saved.remove(pair);
+          }
+        else
+          {
+            _saved.add(pair);
+          }
+      },
     );
   }
 }
